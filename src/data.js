4c2acd4c01356c0a47d9ff47353a17bba905eb91
aadarshmunna221 <aadarshmunnacs@gmail.com>
2026-02-19 22:37:43 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const products = [
     category: "men's clothing",
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     rating: {
-      rate: 4.9,
+      rate: 3.9,
       count: 120,
     },
   },
